3580bf3763f07e10e3b714a315cf966289fa458a
LENOVO <LENOVO@LAPTOP-59AJLO1B>
2025-08-17 20:23:11 +0530

    first commit

---
+++ assignment4/src/assignment4/Author.java
@@ -0,0 +1,32 @@
+package assignment4;
+public class Author {
+	private String name;
+    private String email;
+    private char gender;
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public String toString() {
+        return "Author Name: " + name + ", Email: " + email + ", Gender: " + gender;
+    }
+}
---
+++ assignment4/src/assignment4/Main.java
@@ -0,0 +1,26 @@
+package assignment4;
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);    
+        System.out.print("Enter Author Name: ");
+        String authorName = sc.nextLine();
+        System.out.print("Enter Author Email: ");
+        String authorEmail = sc.nextLine();
+        System.out.print("Enter Author Gender (M/F/O): ");
+        char authorGender = sc.next().charAt(0);
+        Author author = new Author(authorName, authorEmail, authorGender);
+        sc.nextLine(); 
+        System.out.print("Enter Book Name: ");
+        String bookName = sc.nextLine();
+        System.out.print("Enter Book Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Enter Quantity in Stock: ");
+        int qty = sc.nextInt();
+        Book book = new Book(bookName, author, price, qty);
+        System.out.println("\n--- Book Details ---");
+        System.out.println(book);
+        sc.close();
+    }
+}
+
